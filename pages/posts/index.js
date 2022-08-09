bd919c783156613bc8d3ef40ef45f9316db9a90c
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2022-08-09 13:23:01 +0600

    Image component added for blur loading

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -70,14 +70,3 @@ const posts = () => {
 }
 
 export default posts
-
-export const getServerSideProps=async ()=>{
-  const res = await fetch('http://localhost:3000/api/hello')
-  const data = await res.json()
-  console.log(data)
-  return {
-    props:{
-      
-    }
-  }
-}
